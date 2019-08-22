ead25062c5d528382ad3df1f4d6b77841779cc2f
Tracie T <traciecodes@gmail.com>
2019-08-22 16:11:56 -0500

    Updated the form and added addit. functionality. Moved the axios call so it could be used in other components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import UpdateMovie from './Movies/UpdateMovie';
+import AddMovie from './Movies/AddMovie';
+import axios from 'axios';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(results => {
+        console.log(results.data)
+        setMovies(results.data)
+      })
+      .catch(error => console.log(error.response))
+  }, [])
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
@@ -14,15 +28,29 @@ const App = () => {
   return (
     <>
       <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
+      <Route exact path="/" render={props => {
+        return <MovieList {...props} movies={movies}/>
+      }}
+      />
       <Route
         path="/movies/:id"
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
       />
-    </>
-  );
+     <Route
+        path="/update-movie/:id"
+        render={props => {
+          return <UpdateMovie {...props} movies={movies} setMovies={setMovies}/>;
+        }}
+      />
+      <Route
+        path="/add-movie"
+        render={props => {
+          return <AddMovie {...props} />;
+        }}
+      />
+    </>	    
+  );	  
 };
-
 export default App;
